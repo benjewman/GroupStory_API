0d9505e85e28e9c1542be0b266c57dc731c4b324
benjewman <bennyadave@gmail.com>
2020-04-07 21:47:58 -0400

    Add create action to chapters controller

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -3,4 +3,18 @@ class ChaptersController < ApplicationController
         chapters = Chapter.all
         render json: chapters 
     end
+
+    def create
+        chapter = Chapter.new(content: chapter_params[:content], story_id: chapter_params[:story_id])
+        if chapter.save
+            render json: chapter
+        else
+            render json: {message: "could not save"}
+        end
+    end
+
+    private
+    def chapter_params
+        params.require(:chapter).permit(:content, :story_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :chapters, only: [:index]
+  resources :chapters, only: [:index, :create]
   resources :stories, only: [:index, :show]
   get 'last', to: 'stories#last'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
